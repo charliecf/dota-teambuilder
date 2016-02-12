2ac8a226a570520f7b66f760a895520a74b5c89a
charliecf <charlie.feng@mail.mcgill.ca>
2016-02-12 14:22:46 -0500

    main

---
+++ main.py
@@ -0,0 +1,21 @@
+# Hero Input
+heroes = [
+			'doom', 
+			'tusk', 'shadow_fiend', 'queen_of_pain', 'slardar', 'undying', 
+			'juggernaut', 'zeus', 'spirit_breaker', 'lina', 'lich', 'beastmaster']
+
+# Strategy Input
+"""
+Strategies include:
+push
+gank
+teamfight
+"""
+
+"""
+Minimum 3 cores
+"""
+
+
+
+# Players Input
